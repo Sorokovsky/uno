b181fd720678123863eb51a4d48ce97911912a53
Sorokovsky <Sorokovskys@ukr.net>
2024-08-12 21:27:51 +0300

    get random card

--- src/components/pages/Game/Game.tsx
+++ src/components/pages/Game/Game.tsx
@@ -1,7 +1,6 @@
 import { FC, memo } from "react";
 import HandCards from "../../HandCards/HandCards";
 import { useGameStore } from '../../../store/game/game.store';
-import Button from "../../ui/Button/Button";
 import cn from 'clsx';
 import styles from "./Game.module.sass";
 import CurrentCard from "../../CurrentCard/CurrentCard";
