f28b8eae5524e6de391685ddf665af96b7130cea
Sorokovsky <Sorokovskys@ukr.net>
2024-08-12 16:35:38 +0300

    get store type

--- src/types/game.type.ts
+++ src/types/game.type.ts
@@ -2,10 +2,12 @@ import { Card } from "./card.type";
 import { PlayerType } from "./player-type.type";
 import { Player } from './player.type';
 
+export type GetGame = () => GameStore;
+
 export type GameStore = {
     turn: PlayerType;
     player: Player;
     oponent: Player;
-    addCards: (count: number, player: PlayerType) => GameStore;
-    throwCard: (card: Card) => GameStore;
+    addCards: (get: GetGame, count: number, player: PlayerType) => GameStore;
+    throwCard: (get: GetGame, card: Card) => GameStore;
 };
