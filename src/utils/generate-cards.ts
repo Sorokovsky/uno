ba5d16954b8ada8eaebfab790adc4e97c6d9bbed
Sorokovsky <Sorokovskys@ukr.net>
2024-08-13 17:26:02 +0300

    fix cards id

--- src/utils/generate-cards.ts
+++ src/utils/generate-cards.ts
@@ -1,11 +1,13 @@
 import { Cards } from "../constants/cards.constant";
-import { Card } from "../types/card.type";
+import type { Card } from "../types/card.type";
+import { getId } from "./get-id";
 import { random } from "./random";
 
 export const generateCards = (count: number): Card[] => {
     const cards: Card[] = [];
     for (let i = 0; i < count; i++) {
-        cards.push(Cards[random(0, Cards.length - 1)]);
+        const card: Card = Cards[random(0, Cards.length - 1)];
+        cards.push({...card, id: getId()});
     }
     return cards;
 };
