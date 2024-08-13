9d84eeaa187eccee4fbce4d688a5f0f596ef2049
Sorokovsky <Sorokovskys@ukr.net>
2024-08-13 18:37:07 +0300

    is equal card

--- src/components/HandCards/HandCards.tsx
+++ src/components/HandCards/HandCards.tsx
@@ -1,11 +1,38 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect } from 'react';
 import { IHandsCards } from "./HandCards.props";
 import cn from "clsx";
 import styles from "./HandCards.module.sass";
 import PlayerCard from "../PlayerCard/PlayerCard";
 import { getId } from "../../utils/get-id";
+import { useGameStore } from "../../store/game/game.store";
+import type { Card } from '../../types/card.type';
+import { canThrowCard } from '../../utils/canThrowCard';
 
 const HandCards: FC<IHandsCards> = ({ position = 'bottom', cards, playerType }): JSX.Element => {
+    const { turn, oponent, currentCard, throwCard, addCards } = useGameStore();
+    const isOponent = playerType === 'oponent';
+
+    useEffect(() => {
+        const isOponentTurn = isOponent && turn === 'oponent';
+        if (isOponentTurn) {
+            let resultCard: Card | null = null;
+            for (let i = 0; i < oponent.deck.length; i++) {
+                const card: Card = oponent.deck[i];
+                if (canThrowCard(card, currentCard)) {
+                    resultCard = card;
+                    break;
+                }
+            }
+            if (resultCard) {
+                throwCard(resultCard);
+            } else {
+                addCards(1, 'oponent');
+            }
+
+        }
+
+    }, [turn, oponent, playerType]);
+
     return (
         <div className={cn(styles.hand, styles[position], styles[playerType])}>
             {cards.map((card) => { 
